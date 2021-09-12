c76fea4d27e4c0b4742bd6647365b8cdfd2466e1
Rohit Kumar Singh <Rohit.Singh@st.niituniversity.in>
2021-09-13 00:39:31 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -85,9 +85,9 @@ var styles2 = [
     , 'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
+console.log('\n\n%c SAVE THE DATE: 9th Dec, 2021!', styles);
 
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
+console.log('%cYour presence is requested!%c\n\nRegards: Rohit Kumar Singh', styles1, styles2);
 
 console.log(
     `%cShaadi me zaroor aana!\n\n`,
